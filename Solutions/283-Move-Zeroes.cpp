86265f805c6d34f3a9a766cd7b6023509033c5ae
UnrealXinda <51731548+UnrealXinda@users.noreply.github.com>
2020-10-21 23:06:18 -0500

    Create 283-Move-Zeroes.cpp

---
+++ Solutions/283-Move-Zeroes.cpp
@@ -0,0 +1,18 @@
+// Problem link: https://leetcode.com/problems/move-zeroes/
+
+/*
+Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+*/
+
+/*
+Runtime: 8 ms, faster than 83.28% of C++ online submissions for Move Zeroes.
+Memory Usage: 9.5 MB, less than 100.00% of C++ online submissions for Move Zeroes.
+*/
+
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k)
+    {
+        std::stable_partition(nums.begin(), nums.end(), [](int n){ return n != 0; });
+    }
+};
