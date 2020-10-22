7b040ba13613ebe6d091e73a5ec47732af371c48
UnrealXinda <51731548+UnrealXinda@users.noreply.github.com>
2020-10-21 22:24:19 -0500

    Create 169-Majority-Element.cpp

---
+++ Solutions/169-Majority-Element.cpp
@@ -0,0 +1,24 @@
+// Problem link: https://leetcode.com/problems/majority-element/
+
+/*
+Given an array of size n, find the majority element. The majority element is the element that appears more than ⌊ n/2 ⌋ times.
+You may assume that the array is non-empty and the majority element always exist in the array.
+*/
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums)
+    {
+        auto counts = std::unordered_map<int, int>();
+        
+        std::for_each(std::begin(nums),
+                      std::end(nums),
+                      [&](int num) { ++counts[num]; });
+        
+        const auto it = std::max_element(std::begin(counts),
+                                         std::end(counts),
+                                         [](const auto& a, const auto& b) { return a.second < b.second; });
+        
+        return it->first;
+    }
+};
