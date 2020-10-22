092cb6c75b7ed488b2247c515825727de8f88626
UnrealXinda <51731548+UnrealXinda@users.noreply.github.com>
2020-10-21 22:54:11 -0500

    Update 26-Remove-Duplicates-from-Sorted-Array.cpp

--- Solutions/26-Remove-Duplicates-from-Sorted-Array.cpp
+++ Solutions/26-Remove-Duplicates-from-Sorted-Array.cpp
@@ -5,6 +5,11 @@ Given a sorted array nums, remove the duplicates in-place such that each element
 Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
 */
 
+/*
+Runtime: 16 ms, faster than 87.26% of C++ online submissions for Remove Duplicates from Sorted Array.
+Memory Usage: 13.9 MB, less than 99.99% of C++ online submissions for Remove Duplicates from Sorted Array.
+*/
+
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums)
