536e7a9231c349466d75df007fd489cbc4d6ec3a
UnrealXinda <51731548+UnrealXinda@users.noreply.github.com>
2020-10-21 22:54:47 -0500

    Update 35-Search-Insert-Position.cpp

--- Solutions/35-Search-Insert-Position.cpp
+++ Solutions/35-Search-Insert-Position.cpp
@@ -4,6 +4,11 @@
 Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
 */
 
+/*
+Runtime: 8 ms, faster than 82.71% of C++ online submissions for Search Insert Position.
+Memory Usage: 9.9 MB, less than 99.99% of C++ online submissions for Search Insert Position.
+*/
+
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target)
