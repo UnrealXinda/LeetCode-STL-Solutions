da5e3608eb589de19ef73b4adca75b15d1cca15c
UnrealXinda <51731548+UnrealXinda@users.noreply.github.com>
2020-10-21 21:45:21 -0500

    Create 26-Remove-Duplicates-from-Sorted-Array.cpp

---
+++ Solutions/26-Remove-Duplicates-from-Sorted-Array.cpp
@@ -0,0 +1,16 @@
+// Problem link: https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+
+/*
+Given a sorted array nums, remove the duplicates in-place such that each element appears only once and returns the new length.
+Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+*/
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums)
+    {
+        return std::distance(
+            std::begin(nums),
+            std::unique(std::begin(nums), std::end(nums)));
+    }
+};
